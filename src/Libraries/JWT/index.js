75c813cbc3d793862686b451b63191f6679807f2
Cesar Hernandez <devcesarhdez@gmail.com>
2022-03-13 23:07:55 +0000

    generate and verify token library

---
+++ src/Libraries/JWT/index.js
@@ -0,0 +1,12 @@
+import jsonwebtoken from "jsonwebtoken"
+import env from "../../Config/env"
+
+export default const jwt = {
+	sign: async ( data ) => {
+		return await jsonwebtoken.sign( data, env.SECRET, { expiresIn: "1h" } )
+	},
+
+	verify: async ( token ) => {
+		return await jsonwebtoken.verify( token, env.SECRET )
+	}
+}
